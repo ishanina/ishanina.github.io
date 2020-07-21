69a0874315e84b76803601405b009246dc686e45
Ishan Levy <ishanlevy97@gmail.com>
2020-07-20 22:43:49 -0400

    Added links

--- htmllinker.sh
+++ htmllinker.sh
@@ -35,5 +35,20 @@ find ./Writings -name "*exceptions.txt" -maxdepth 3 |while read file; do
 done
 }
 
-
-
+outfile="index.md"
+echo > "$outfile"
+find "." -mindepth 1 -maxdepth 1 -type d ! -name ".git"|while read folder; do
+    echo "$folder"
+    echo "## ${folder##*/}" >> "$outfile"
+    find "$folder" -name "*.html" -maxdepth 3 |while read file; do  
+        relpath="${file#*/}"
+        linkname="${relpath#*/}"
+        linkname="${linkname%/*}"
+        echo "$relpath"
+        echo "$linkname"
+        pdflink="${file/.\//https://github.com/ishanina/PublicWritings/blob/master/}"
+        pdflink="${pdflink/ /%20}"
+        echo "$pdflink"
+        echo "[$linkname]($relpath) [(pdf)]($pdflink)" >> "$outfile"
+    done
+done

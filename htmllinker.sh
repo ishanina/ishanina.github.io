d08a889563bcb1bcacfe328cd43fb9af80138484
Ishan Levy <ishanlevy97@gmail.com>
2020-07-20 22:53:43 -0400

    fixed spacing

--- htmllinker.sh
+++ htmllinker.sh
@@ -15,6 +15,7 @@ find "." -mindepth 1 -maxdepth 1 -type d ! -name ".git"|while read folder; do
         pdflink=$(sed 's/_/%20/g' <<< "$pdflink")
         pdflink=$(sed 's/.html/.pdf/g' <<< "$pdflink")
         echo "$pdflink"
+        echo >> "$outfile"
         echo "[$linkname]($relpath) [(pdf)]($pdflink)" >> "$outfile"
     done
 done
